46f84526e068f0d9ceaa7f3065086e34373341f1
Abdirahman-Dirie <maankacade1557@gmail.com>
2024-03-31 21:15:18 +0300

    deleted damiin app

--- Construction_MT/Backend/construction_venv/contruction_system/damiin/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Damiin(models.Model):
-    Name = models.CharField(max_length=200)
-    Document = models.CharField(max_length = 100000)
--- Construction_MT/Backend/construction_venv/contruction_system/damiin/searilizer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Damiin
-
-class DamiinSerializers(serializers.ModelSerializer):
-    class Meta:
-        model=Damiin
-        fields='__all__'
