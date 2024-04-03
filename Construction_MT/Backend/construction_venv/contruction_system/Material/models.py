f2bd23630718ef25e668b4c20247141ed7bc39a4
Abdirahman-Dirie <maankacade1557@gmail.com>
2024-04-03 04:14:49 +0300

    wuxuu igu qasbay inaan ku soo daro default=None

--- Construction_MT/Backend/construction_venv/contruction_system/Material/models.py
+++ Construction_MT/Backend/construction_venv/contruction_system/Material/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from Projects.models import  Projects
 # Create your models here.
 class Material(models.Model):
-     project=models.ForeignKey(Projects,on_delete=models.CASCADE)
+     project=models.ForeignKey(Projects,on_delete=models.CASCADE,default=None)
      material_name=models.CharField(max_length=100)
      quantity = models.IntegerField()
      unit_price=models.DecimalField(max_digits=5, decimal_places=2) 
--- Construction_MT/Backend/construction_venv/contruction_system/Payments/models.py
+++ Construction_MT/Backend/construction_venv/contruction_system/Payments/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from Projects.models import Projects
-from Client.models import Client
 from Payment_Methode.models import  Payment_Methode
 # Create your models here.
 class Payments(models.Model):
-    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
-    payment_method=models.ForeignKey(Payment_Methode,on_delete=models.CASCADE)
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE, default=None)
+    payment_method=models.ForeignKey(Payment_Methode,on_delete=models.CASCADE, default=None)
     amount=models.DecimalField(max_digits=10, decimal_places=2)
     issue_date = models.DateField() 
     
