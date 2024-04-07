be88c6ae92676d180e5df8de1e159fa8e55d8cb4
Abdirahman-Dirie <maankacade1557@gmail.com>
2024-04-07 07:41:57 +0300

    waxaan ilaaway inaan daleedo budget haddda waan daleeday

--- Construction_MT/Backend/construction_venv/contruction_system/Budget/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-# Create your models here.
-class Budget(models.Model):
-    size=models.CharField(max_length=100)
-    quntinty=models.IntegerField()
-    Budget=models.DecimalField(max_digits=20, decimal_places=2)
-    issue_date = models.DateField(null=True)
