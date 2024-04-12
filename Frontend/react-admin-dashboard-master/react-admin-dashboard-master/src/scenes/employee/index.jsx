70eef817936547bca18888d90c1fdfdc2893c976
Abdirahman-Dirie <maankacade1557@gmail.com>
2024-04-12 16:54:41 +0300

    waa ogahay waxba qasay

--- Frontend/react-admin-dashboard-master/react-admin-dashboard-master/src/scenes/employee/index.jsx
+++ Frontend/react-admin-dashboard-master/react-admin-dashboard-master/src/scenes/employee/index.jsx
@@ -1,8 +1,10 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Typography, useTheme, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const Employee = () => {
   const [employees, setEmployees] = useState([]);
@@ -12,10 +14,15 @@ const Employee = () => {
   }, []);
 
   const fetchEmployees = async () => {
+    console.log("Fetching employees...");
     try {
-      const response = await fetch('http://127.0.0.1:8000/Employee/'); 
+      const response = await fetch('http://127.0.0.1:8000/Employee/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
       const data = await response.json();
       setEmployees(data);
+      console.log("Fetched employees:", data);
     } catch (error) {
       console.error('Error fetching employees:', error);
     }
@@ -61,8 +68,13 @@ const Employee = () => {
   return (
     <Box m="20px">
       <Header title="Employee" subtitle="List of Employee Balances" />
+      
+      <Link to="create" style={{ textDecoration: 'none' }}>
+        <Button variant="contained" color="primary" style={{ marginBottom: '20px' }}>
+          Add Employee
+        </Button>
+      </Link>
       <Box
-        m="40px 0 0 0"
         height="75vh"
         sx={{
           "& .MuiDataGrid-root": {
