606af7bc363921aeee57fef5b3cd8f0b1d20f1b0
Abdirahman-Dirie <maankacade1557@gmail.com>
2024-04-21 21:56:58 +0300

    after long time error i solved alx

--- Construction_MT/Backend/construction_venv/contruction_system/Client/models.py
+++ Construction_MT/Backend/construction_venv/contruction_system/Client/models.py
@@ -6,6 +6,7 @@ class Client(models.Model):
     client_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     document = models.CharField(max_length=100,default="Unknown")
+    balance = models.DecimalField(max_digits= 10, decimal_places=2,default=0) 
     issue_date = models.DateField(default=datetime.date.today)
 
 
