4e53afbf133d22f40457c336f63c7892e9d455ce
Abdirahman-Dirie <maankacade1557@gmail.com>
2024-06-06 10:57:07 +0300

    Table comapny added 2 column phone and about

--- Construction_MT/Backend/construction_venv/contruction_system/companies/models.py
+++ Construction_MT/Backend/construction_venv/contruction_system/companies/models.py
@@ -6,6 +6,8 @@ class Companies(models.Model):
     company_name = models.CharField(max_length=100)
     company_logo = models.ImageField(max_length=100)
     company_address = models.CharField(max_length=100)
+    company_phone = models.CharField(max_length=100, default=None)
+    company_about = models.CharField(max_length=100 , default=None)
     email = models.EmailField(unique=True, default=None) 
     # user = models.ForeignKey(user,on_delete=models.CASCADE, default=None)  
     issue_date = models.DateField(default=datetime.date.today)
